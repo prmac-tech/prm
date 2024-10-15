6478f61f30e7c2349d00dbb730d060f3b8628fb3
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-10-15 18:02:55 +0100

    Added K8s terraform

--- .github/terraform.tfvars
+++ .github/terraform.tfvars
@@ -1,4 +1,4 @@
-resource_group_name = "aks_terraform_rg"
+
 cluster_name        = "aks-terraform-cluster"
 kubernetes_version  = "1.31.1"
 system_node_count   = 2
--- .github/variables.tf
+++ .github/variables.tf
@@ -1,29 +1,35 @@
 variable "resource_group_name" {
   type        = string
   description = "Resource Group name in Microsoft Azure"
+  default = "aks_terraform_rg"
 }
 
 variable "location" {
   type        = string
   description = "Resources location in Microsoft Azure"
+  default = azurerm_resource_group.rg.location
 }
 
 variable "cluster_name" {
   type        = string
   description = "AKS name in Microsoft Azure"
+  default = "aks-terraform-cluster"
 }
 
 variable "kubernetes_version" {
   type        = string
   description = "Kubernetes version"
+  default = "1.31.1"
 }
 
 variable "system_node_count" {
   type        = number
   description = "Number of AKS worker nodes"
+  default = 2
 }
 
 variable "node_resource_group" {
   type        = string
   description = "Resource Group name for cluster resources in Microsoft Azure"
+  default = "aks_terraform_node_resources_rg"
 }

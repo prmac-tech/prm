25deff2f8807bc1a790184ca038aec2500e6b461
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-10-15 18:23:16 +0100

    Added K8s terraform

--- .github/providers.tf
+++ .github/providers.tf
@@ -5,6 +5,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
+    use_oidc = true
     resource_group_name  = "prm"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "prmstorageacc"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.

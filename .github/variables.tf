2301452f698973309d037c1610cfc199c86878d6
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-10-16 12:45:52 +0100

    Added K8s terraform

--- .github/variables.tf
+++ .github/variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   type        = string
   description = "Resource Group name in Microsoft Azure"
-  default = "prm"
+  default = "prmk8s"
 }
 
 variable "location" {

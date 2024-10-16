0a6e3a1505f051eb6b1a997afeef9b0d08cd2e42
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-10-16 12:42:41 +0100

    Added K8s terraform

--- .github/main.tf
+++ .github/main.tf
@@ -1,13 +1,8 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.cluster_name
   kubernetes_version  = var.kubernetes_version
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = "prm"
   dns_prefix          = var.cluster_name
   node_resource_group = var.node_resource_group
 

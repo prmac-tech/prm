e0d1ada5a9740c6288b086986e7d2d00a9702ab1
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-10-18 17:04:37 +0100

    Added lets encrypt

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -34,24 +34,24 @@ provider "azurerm" {
   resource_provider_registrations = "none"
 }
 provider "kubernetes" {
-#  host                   = azurerm_kubernetes_cluster.aks.kube_admin_config.0.host
-#  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_certificate)
-#  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_key)
-#  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.cluster_ca_certificate)
+  host                   = azurerm_kubernetes_cluster.aks.kube_admin_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.cluster_ca_certificate)
 }
 provider "helm" {
   debug = true
   kubernetes {
-#    host                   = azurerm_kubernetes_cluster.aks.kube_admin_config.0.host
-#    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_certificate)
-#    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_key)
-#    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.cluster_ca_certificate)
+    host                   = azurerm_kubernetes_cluster.aks.kube_admin_config.0.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.cluster_ca_certificate)
   }
 }
 provider "kubectl" {
-#  host                   = azurerm_kubernetes_cluster.aks.kube_admin_config.0.host
-#  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_certificate)
-#  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_key)
-#  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.cluster_ca_certificate)
+  host                   = azurerm_kubernetes_cluster.aks.kube_admin_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.cluster_ca_certificate)
   load_config_file       = false
 }

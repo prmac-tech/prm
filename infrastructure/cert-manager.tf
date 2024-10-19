d7ad2d8fe0b2e1d05244e364f59bbb70dc8582fc
paulmcphee <paul.mcphee@gmail.com>
2024-10-19 13:01:58 +0100

    Added certifcates

--- infrastructure/cert-manager.tf
+++ infrastructure/cert-manager.tf
@@ -35,15 +35,15 @@ locals {
 # Create clusterissuer for nginx YAML file
 data "kubectl_file_documents" "clusterissuer" {
   content = file(local.clusterissuer)
-  depends_on = [
-    azurerm_kubernetes_cluster.aks
-  ]
 }
 
 # Apply clusterissuer for nginx YAML file
 resource "kubectl_manifest" "clusterissuer" {
   for_each  = data.kubectl_file_documents.clusterissuer.manifests
   yaml_body = each.value
+  depends_on = [
+    azurerm_kubernetes_cluster.aks
+  ]
 }
 
 data "kubectl_file_documents" "certificates" {

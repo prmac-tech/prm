acdaa1b18f5dd7b7999b91f59fd0a23d2b05a0db
paulmcphee <paul.mcphee@gmail.com>
2024-10-21 22:13:23 +0100

    Added regcred

--- infrastructure/secrets.tf
+++ infrastructure/secrets.tf
@@ -16,7 +16,7 @@ resource "kubernetes_secret" "docker-registry" {
 
 
 data "template_file" "docker_config_script" {
-  template = "${file("${path.module}/config.json")}"
+  template = "${file("config.json")}"
   vars = {
     docker-username           = "${var.docker_username}"
     docker-password           = "${var.docker_password}"

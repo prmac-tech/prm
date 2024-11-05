25e8b67476df2282d09af42f48ffca020673ace5
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-11-05 10:30:06 +0000

    Added keycloak

--- infrastructure/auth.tf
+++ infrastructure/auth.tf
@@ -12,7 +12,6 @@ resource "helm_release" "keycloak" {
     })
   ]
   depends_on = [
-    kubernetes_persistent_volume.k8s-pv,
     kubectl_manifest.certificates,
     azurerm_kubernetes_cluster.aks
   ]

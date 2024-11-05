609e9d15c66e892229b98d23056021273fbc497c
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-11-05 10:28:32 +0000

    Added keycloak

--- infrastructure/pvc.tf
+++ infrastructure/pvc.tf
@@ -1,28 +1,3 @@
-resource "kubernetes_persistent_volume" "k8s-pv" {
-  metadata {
-    name = "k8s-pv"
-  }
-  spec {
-    capacity = {
-      storage = "1Gi"
-    }
-    access_modes = ["ReadWriteOnce"]
-    storage_class_name = "default"
-
-    persistent_volume_source {
-      azure_disk {
-        caching_mode  = "None"
-        data_disk_uri = azurerm_managed_disk.k8s-disk.id
-        disk_name     = "k8s"
-        kind          = "Managed"
-      }
-    }
-  }
-  depends_on = [
-    azurerm_kubernetes_cluster.aks
-  ]
-}
-
 resource "kubernetes_persistent_volume_claim" "k8s-pvc" {
   metadata {
     name = "k8s-pvc"
@@ -42,18 +17,3 @@ resource "kubernetes_persistent_volume_claim" "k8s-pvc" {
   ]
 }
 
-
-resource "azurerm_managed_disk" "k8s-disk" {
-  name                 = "k8s-disk"
-  location             = azurerm_resource_group.rg.location
-  resource_group_name  = azurerm_resource_group.rg.name
-  storage_account_type = "StandardSSD_LRS"
-  create_option        = "Empty"
-  disk_size_gb         = "1"
-  tags = {
-    environment = azurerm_resource_group.rg.name
-  }
-  depends_on = [
-    azurerm_kubernetes_cluster.aks
-  ]
-}

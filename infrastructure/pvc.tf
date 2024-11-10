e41b318bd6555adf436402919df6c6ae6c5f6b4f
paulmcphee <paul.mcphee@gmail.com>
2024-11-10 15:18:58 +0000

    PVC Changes

--- infrastructure/pvc.tf
+++ infrastructure/pvc.tf
@@ -1,3 +1,31 @@
+data azurerm_managed_disk azure_disk_prm {
+  name = "prm"
+  resource_group_name = "prm"
+}
+
+resource "kubernetes_persistent_volume" "pv-azuredisk" {
+  metadata {
+    annotations = "pv.kubernetes.io/provisioned-by: disk.csi.azure.com"
+    name = "pv-azuredisk"
+  }
+  spec {
+    capacity {
+      storage = "2Gi"
+    }
+    access_modes = ["ReadWriteOnce"]
+    persistent_volume_reclaim_policy = "Retain"
+    storage_class_name: "managed-csi"
+    persistent_volume_source {
+      azure_disk {
+        caching_mode  = "None"
+        data_disk_uri = data.azurerm_managed_disk.azure_disk_prm.id
+        disk_name     = "prm"
+        kind          = "Managed"
+      }
+    }
+  }
+}
+
 resource "kubernetes_persistent_volume_claim" "prm-pvc" {
   metadata {
     name = "prm"

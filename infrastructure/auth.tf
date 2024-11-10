3b9989683feb48b87d89ed7b969b936065de5ef5
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-11-10 13:32:19 +0000

    Temp remove keycloak

--- infrastructure/auth.tf
+++ infrastructure/auth.tf
@@ -1,18 +1,18 @@
 
-resource "helm_release" "keycloak" {
-  name       = "auth"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "keycloak"
-  namespace  = "prm"
-  version    = "24.0.2"
-
-  values = [
-    templatefile("${path.root}/auth/values.yaml", {
-      admin_password  = "test"
-    })
-  ]
-  depends_on = [
-    kubectl_manifest.certificates,
-    azurerm_kubernetes_cluster.aks
-  ]
-}
+#resource "helm_release" "keycloak" {
+#  name       = "auth"
+#  repository = "https://charts.bitnami.com/bitnami"
+#  chart      = "keycloak"
+#  namespace  = "prm"
+#  version    = "24.0.2"
+#
+#  values = [
+#    templatefile("${path.root}/auth/values.yaml", {
+#      admin_password  = "test"
+#    })
+#  ]
+#  depends_on = [
+#    kubectl_manifest.certificates,
+#    azurerm_kubernetes_cluster.aks
+#  ]
+#}
--- infrastructure/pvc.tf
+++ infrastructure/pvc.tf
@@ -1,19 +1,30 @@
-resource "kubernetes_persistent_volume_claim" "k8s-pvc" {
-  metadata {
-    name = "k8s-pvc"
-    namespace  = "prm"
-  }
-  spec {
-    access_modes = ["ReadWriteOnce"]
-    resources {
-      requests = {
-        storage = "1Gi"
-      }
-    }
-    storage_class_name = "default"
-  }
-  depends_on = [
-    azurerm_kubernetes_cluster.aks
-  ]
-}
+#resource "kubernetes_persistent_volume_claim" "k8s-pvc" {
+#  metadata {
+#    name = "k8s-pvc"
+#    namespace  = "prm"
+#  }
+#  spec {
+#    access_modes = ["ReadWriteOnce"]
+#    resources {
+#      requests = {
+#        storage = "1Gi"
+#      }
+#    }
+#    storage_class_name = "default"
+#  }
+#  depends_on = [
+#    azurerm_kubernetes_cluster.aks
+#  ]
+#}
 
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+name: azure-managed-disk
+spec:
+accessModes:
+- ReadWriteOnce
+storageClassName: managed-csi
+resources:
+requests:
+storage: 5Gi

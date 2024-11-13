86d565ab86aeeb1c4465e4cf233196678ecf1fdd
paulmcphee <paul.mcphee@gmail.com>
2024-11-13 20:33:18 +0000

    Change auth to use external keycloak

--- infrastructure/auth.tf
+++ infrastructure/auth.tf
@@ -1,18 +1,20 @@
 
-#resource "helm_release" "keycloak" {
-#  name       = "auth"
-#  repository = "https://charts.bitnami.com/bitnami"
-#  chart      = "keycloak"
-#  namespace  = "prm"
-#  version    = "24.0.2"
-#
-#  values = [
-#    templatefile("${path.root}/auth/values.yaml", {
-#      admin_password  = "test"
-#    })
-#  ]
-#  depends_on = [
-#    kubectl_manifest.certificates,
-#    azurerm_kubernetes_cluster.aks
-#  ]
-#}
+resource "helm_release" "keycloak" {
+  name       = "auth"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "keycloak"
+  namespace  = "prm"
+  version    = "24.0.2"
+
+  values = [
+    templatefile("${path.root}/auth/values.yaml", {
+      admin_password  = "test"
+      postgres_password = var.postgresql_password
+    })
+  ]
+  depends_on = [
+    kubectl_manifest.certificates,
+    azurerm_kubernetes_cluster.aks,
+    helm_release.keycloak
+  ]
+}

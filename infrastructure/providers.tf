7910a9a6a5d7309356ee0abd2e55f9b9ddbadcdc
paulmcphee <paul.mcphee@gmail.com>
2024-11-15 23:29:27 +0000

    Fix K8s config

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -37,6 +37,7 @@ provider "azurerm" {
 }
 
 provider "kubernetes" {
+  load_config_file       = "false"
   host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)

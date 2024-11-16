a96853cdfbadd3cebb03ee1eab99fe5385daa17e
paulmcphee <paul.mcphee@gmail.com>
2024-11-16 00:30:58 +0000

    Fix K8s config

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -52,6 +52,7 @@ provider "helm" {
   }
 }
 provider "kubectl" {
+  load_config_file       = "false"
   host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)

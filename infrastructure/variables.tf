da7610142f97559405888f0f67e1fe0ed9b4cde3
paulmcphee <paul.mcphee@gmail.com>
2025-10-09 19:34:33 +0100

    Update K8s version

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -44,7 +44,7 @@ variable "cluster_name" {
 variable "kubernetes_version" {
   type        = string
   description = "Kubernetes version"
-  default = "1.30.5"
+  default = "1.34.1"
 }
 
 variable "system_node_count" {

45ede619f7f4f1303c5cb0555a211c77bfa1d6ab
paulmcphee <paul.mcphee@gmail.com>
2025-10-16 18:51:49 +0100

    Update postgres chart

--- infrastructure/auth.tf
+++ infrastructure/auth.tf
@@ -1,31 +1,31 @@
-
-resource "helm_release" "keycloak" {
-  name       = "auth"
-  chart      = "${path.module}/../helm/charts/helm_charts/keycloak"
-  namespace  = "prm"
-  version    = "2.0.0-dataplane-advertise-version-alpha-56e2291"
-  timeout    = "600"
-
-  values = [
-    templatefile("${path.root}/auth/values.yaml", {
-      admin_password  = "test"
-      postgres_password = var.postgresql_password
-    })
-  ]
-
-#  set {
-#    name  = "image.repository"
-#    value = "bitnamilegacy"
-#  }
 #
-#  set {
-#    name  = "global.security.allowInsecureImages"
-#    value = "true"
-#  }
-
-  depends_on = [
-    kubectl_manifest.certificates,
-    azurerm_kubernetes_cluster.aks,
-    helm_release.postgresql
-  ]
-}
+#resource "helm_release" "keycloak" {
+#  name       = "auth"
+#  chart      = "${path.module}/../helm/charts/helm_charts/keycloak"
+#  namespace  = "prm"
+#  version    = "2.0.0-dataplane-advertise-version-alpha-56e2291"
+#  timeout    = "600"
+#
+#  values = [
+#    templatefile("${path.root}/auth/values.yaml", {
+#      admin_password  = "test"
+#      postgres_password = var.postgresql_password
+#    })
+#  ]
+#
+##  set {
+##    name  = "image.repository"
+##    value = "bitnamilegacy"
+##  }
+##
+##  set {
+##    name  = "global.security.allowInsecureImages"
+##    value = "true"
+##  }
+#
+#  depends_on = [
+#    kubectl_manifest.certificates,
+#    azurerm_kubernetes_cluster.aks,
+#    helm_release.postgresql
+#  ]
+#}
